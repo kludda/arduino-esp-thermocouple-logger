d73b2e995a9f2f1ce17c06c81b623460f979c409
kludda <david.alind@gmail.com>
2024-08-21 20:07:47 +0200

    rewritten for max31856
    compiling but not tested

--- thermocouple_logger/thermocouple_logger.ino
+++ thermocouple_logger/thermocouple_logger.ino
@@ -5,10 +5,7 @@ Code for the webserver and websocket was found here
 https://randomnerdtutorials.com/esp8266-nodemcu-websocket-server-arduino/
 
 Other useful resources:
-https://www.analog.com/media/en/technical-documentation/data-sheets/ds18b20.pdf
 https://arduino-esp8266.readthedocs.io/en/latest/index.html
-https://www.pjrc.com/teensy/td_libs_OneWire.html
-https://github.com/milesburton/Arduino-Temperature-Control-Library
 
 
 
@@ -22,9 +19,8 @@ https://github.com/milesburton/Arduino-Temperature-Control-Library
 //#include <ESP8266mDNS.h>
 #include <AsyncElegantOTA.h>
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
-
+#include <SPI.h>
+#include <Adafruit_MAX31856.h>
 
 #include "credentials.h"
 #include "html.h"
@@ -32,16 +28,17 @@ https://github.com/milesburton/Arduino-Temperature-Control-Library
 #include "kludda_wifi.h"
 #include "kludda_mqtt.h"
 
-// one wire Data wire 
-#define ONE_WIRE_BUS D3
+// use hardware SPI, just pass in the CS pin
+Adafruit_MAX31856 maxthermo[] = {
+  Adafruit_MAX31856(D0), 
+  Adafruit_MAX31856(D1), 
+  Adafruit_MAX31856(D2)
+  };
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+int num_sensors = 3;
+bool conversion_started[3] = {false, false, false};
 
-int num_sensors = 0;
 
 
 // webserver
@@ -54,7 +51,7 @@ AsyncWebSocket ws("/ws");
 
 unsigned long lastsendtime = 0;
 
-  
+
 
 
 
@@ -81,7 +78,7 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo *)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     String message = "";
-    for(int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) {
       message += (char)data[i];
     }
 
@@ -90,7 +87,7 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
       String key = message.substring(0, pos);
       String value = message.substring(pos + 1);
 
-/*
+      /*
       if (key == "dutycycle") {
         set_dc(dutycycle);
       }
@@ -120,9 +117,8 @@ void mqtt_on_message(char* topic, byte* payload, unsigned int length) {
 }
 */
 
-String processor(const String& var)
-{
-  if(var == "HEADER")
+String processor(const String &var) {
+  if (var == "HEADER")
     return header_html;
   else if (var == "FOOTER")
     return footer_html;
@@ -165,7 +161,7 @@ t_subscribe subs[] = {
 */
 
 void setup() {
-//  randomSeed(micros());
+  //  randomSeed(micros());
 
   Serial.begin(115200);
   delay(100);
@@ -179,13 +175,13 @@ void setup() {
   setup_wifi();
 
 
-/*
+  /*
   Serial.print("subs len.");
   Serial.println(sizeof(subs) / sizeof(t_subscribe));
 */
-  // setup wifi 
-  setup_mqtt(); //subs);
-/*
+  // setup wifi
+  setup_mqtt();  //subs);
+                 /*
   mqtt_subscribe("node/prop1/set",  on_mqtt_prop1_set);
   mqtt_subscribe("node/prop2/set",  on_mqtt_prop2_set);
 */
@@ -205,7 +201,7 @@ void setup() {
   });
 
 
-/*
+  /*
 {
   "messages": [
     { "type": "ok", "text": "Configuration saved."}
@@ -216,15 +212,15 @@ void setup() {
   ]
 }
 */
-//TODO: change to String response (see wifiscan)
+  //TODO: change to String response (see wifiscan)
   // setup /conf GET
   server.on("/conf", HTTP_GET, [](AsyncWebServerRequest *request) {
     AsyncResponseStream *response = request->beginResponseStream("application/json");
     response->print(F("{"));
     int params = request->params();
-    for(int i=0;i<params;i++){
-      AsyncWebParameter* p = request->getParam(i);
-      if(!p->isFile()){
+    for (int i = 0; i < params; i++) {
+      AsyncWebParameter *p = request->getParam(i);
+      if (!p->isFile()) {
         Serial.print("set conf: ");
         Serial.print(p->name().c_str());
         Serial.print(", ");
@@ -233,7 +229,7 @@ void setup() {
       }
     }
 
-    if(params > 0 && conf_initalized) {
+    if (params > 0 && conf_initalized) {
       response->print(F("\"messages\": [ "));
       response->print(F("{\"type\":\"ok\", \"text\":\"Configuration saved.\"}"));
       response->print(F("],"));
@@ -241,16 +237,16 @@ void setup() {
 
 
     response->print(F("\"conf\": [ "));
-    if(conf_initalized) {
-      int len = sizeof(conf)/sizeof(t_conf);
+    if (conf_initalized) {
+      int len = sizeof(conf) / sizeof(t_conf);
       for (int i = 0; i < len; i++) {
-        if(!conf[i].hidden) {
+        if (!conf[i].hidden) {
           response->print(F("{ \"name\": \""));
           response->print(conf[i].name);
           response->print(F("\", \"value\": \""));
           response->print(conf[i].data);
           response->print(F("\" }"));
-          if(i < len - 1) {
+          if (i < len - 1) {
             response->print(F(","));
           }
         }
@@ -261,13 +257,13 @@ void setup() {
     request->send(response);
   });
 
-    // setup /wifi
+  // setup /wifi
   server.on("/wifi", HTTP_GET, [](AsyncWebServerRequest *request) {
     AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", wifi_html, processor);
     request->send(response);
   });
 
-    // setup /mqtt
+  // setup /mqtt
   server.on("/mqtt", HTTP_GET, [](AsyncWebServerRequest *request) {
     AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", mqtt_html, processor);
     request->send(response);
@@ -288,8 +284,8 @@ void setup() {
     ]
   }
   */
-// https://github.com/me-no-dev/ESPAsyncWebServer/issues/85
-  server.on("/wifiscan", HTTP_GET, [](AsyncWebServerRequest *request){
+  // https://github.com/me-no-dev/ESPAsyncWebServer/issues/85
+  server.on("/wifiscan", HTTP_GET, [](AsyncWebServerRequest *request) {
     String json = "{";
 
     // check if we need to send messages
@@ -299,24 +295,24 @@ void setup() {
       json += F("{\"type\":\"fail\", \"text\":\"Warning: Cannot scan for networks when connected to network. SSID list might be outdated. Disable WiFi and try again.\"}");
       m++;
     }
-    
+
     int n = lastnetworkscan;
 
-    if (n<1) {
-      if(m) json += F(",");
+    if (n < 1) {
+      if (m) json += F(",");
       json += F("{\"type\":\"fail\", \"text\":\"No networks found.\"}");
     }
 
     json += F("]");
 
     // build network json
-    if(n) {
+    if (n) {
       json += F(", \"networks\": [ ");
-      for (int i = 0; i < n; ++i){
-        if(i) json += F(",");
+      for (int i = 0; i < n; ++i) {
+        if (i) json += F(",");
         json += F("{");
-        json += "\"ssid\":\""+WiFi.SSID(i)+"\"";
-        json += ", \"rssi\":\""+String(WiFi.RSSI(i))+"\"";
+        json += "\"ssid\":\"" + WiFi.SSID(i) + "\"";
+        json += ", \"rssi\":\"" + String(WiFi.RSSI(i)) + "\"";
         json += F("}");
       }
       json += F("]");
@@ -328,7 +324,7 @@ void setup() {
       WiFi.scanNetworks(true);
     }
 
-    json += F("}");    
+    json += F("}");
     request->send(200, F("text/json"), json);
     json = String();
   });
@@ -343,17 +339,17 @@ void setup() {
     ]
   }
   */
-  server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request) {
     String json = "{";
 
     json += F("\"status\": [ ");
 
     json += F("{\"name\":\"wifi_status\",");
     json += F("\"text\":\"WiFi: ") + get_wifi_status_str() + F("\"}");
-	  json += F(",");
+    json += F(",");
     json += F("{\"name\":\"mqtt_status\",");
     json += F("\"text\":\"MQTT: ") + get_mqtt_status_str() + F("\"}");
-	  json += F(",");
+    json += F(",");
     json += F("{\"name\":\"client_id\",");
     json += F("\"text\":\"Client ID: ") + String(unique_id_str) + F("\"}");
     json += F(",");
@@ -361,7 +357,7 @@ void setup() {
     json += F("\"text\":\"Free memory: ") + String(ESP.getFreeHeap()) + F("\"}");
 
     json += F("]");
-    json += F("}");    
+    json += F("}");
 
     request->send(200, F("text/json"), json);
 
@@ -369,105 +365,84 @@ void setup() {
   });
 
   // Start ElegantOTA
-  AsyncElegantOTA.begin(&server);  
+  AsyncElegantOTA.begin(&server);
   //AsyncElegantOTA.begin(&server, "username", "password");
 
   // Start web server
   server.begin();
 
 
-  // Start up the library
-  sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
-delay(100);
-  // locate devices on the bus
-/*
-  Serial.print("Locating devices...");
-  Serial.print("Found ");
-  num_sensors = sensors.getDeviceCount();
-  Serial.print(num_sensors, DEC);
-  Serial.println(" devices.");
-*/
-
-}
-
-
-// function to print a device address
-String ow_address_str(DeviceAddress deviceAddress)
-{
-  String astr = "";
-
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    // zero pad the address if necessary
-    if (deviceAddress[i] < 16) Serial.print("0");
-    astr += String(deviceAddress[i], HEX);
+  for (int i = 0; i < num_sensors; i++) {
+    if (!maxthermo[i].begin()) {
+      Serial.print("Could not initialize thermocouple ");
+      Serial.println(i);
+    }
   }
-  return astr;
-}
-
-
-// function to print the temperature for a device
-String ow_temp_str(DeviceAddress deviceAddress)
-{
-  String tstr = "";
-  float tempC = sensors.getTempC(deviceAddress);
-  if(tempC != DEVICE_DISCONNECTED_C) 
-  {
-    tstr = String(tempC, 2);
+  
+  for (int i = 0; i < num_sensors; i++) {
+    maxthermo[i].setThermocoupleType(MAX31856_TCTYPE_K);
   }
-  return tstr;
+
+  for (int i = 0; i < num_sensors; i++) {
+    maxthermo[i].setConversionMode(MAX31856_ONESHOT_NOWAIT);
+  }
 }
 
 
-
-DeviceAddress tempadr;
 
 void loop() {
   wifi_loop();
   mqtt_loop();
 
 
-
-  // Push 
-  if (lastsendtime + 1000 < millis()) {
-
-  Serial.print("Locating devices...");
-  Serial.print("Found ");
-  num_sensors = sensors.getDeviceCount();
-  Serial.print(num_sensors, DEC);
-  Serial.println(" devices.");
-
-    for(int i = 0; i< num_sensors; i++) {
-      if (sensors.getAddress(tempadr,i)) {
-    //    sensors.getTempCByIndex(i)
-        String t = ow_address_str(tempadr);
-        String p = ow_temp_str(tempadr);
-
-        ws.textAll("{\"id\":\"" + t + "\",\"value\":\"" + p + "\"}");
-        t += "/temperature";
-        mqtt_publish(t, p);
-        delay(10);
-      }
+  for (int i = 0; i < num_sensors; i++) {
+    if(!conversion_started[i]) {
+      maxthermo[i].triggerOneShot();
+      conversion_started[i] = true;
     }
+  }
 
+
+  for (int i = 0; i < num_sensors; i++) {
+    if (maxthermo[i].conversionComplete()) {
+      conversion_started[i] = false;
+
+      uint8_t fault = maxthermo[i].readFault();
+      if (fault) {
+        String json = "{";
+        json += F("\"messages\": [ ");
+        json += F("{\"type\":\"fail\", \"text\":\"Thermocouple");
+        json += i;
+        json += F(" fault: "); 
+        if (fault & MAX31856_FAULT_CJRANGE) json += "Cold Junction Range Fault";
+        if (fault & MAX31856_FAULT_TCRANGE) json += "Thermocouple Range Fault";
+        if (fault & MAX31856_FAULT_CJHIGH)  json += "Cold Junction High Fault";
+        if (fault & MAX31856_FAULT_CJLOW)   json += "Cold Junction Low Fault";
+        if (fault & MAX31856_FAULT_TCHIGH)  json += "Thermocouple High Fault";
+        if (fault & MAX31856_FAULT_TCLOW)   json += "Thermocouple Low Fault";
+        if (fault & MAX31856_FAULT_OVUV)    json += "Over/Under Voltage Fault";
+        if (fault & MAX31856_FAULT_OPEN)    json += "Thermocouple Open Fault";
+        json += F("\"}");
+        json += F("]");
+        ws.textAll(json);
+        json = String();
+      }
+
+      String t = "thermocouple" + i;
+      String p = String(maxthermo[i].readThermocoupleTemperature(), 2);
+
+      ws.textAll("{\"id\":\"" + t + "\",\"value\":\"" + p + "\"}");
+      t += "/temperature";
+      mqtt_publish(t, p);
+      delay(10);
+    }
+  }
+
+
+  if (lastsendtime + 1000 < millis()) {
     mqtt_publish("mcu/freeheap", String(ESP.getFreeHeap()));
-/*
-    Serial.println("RPM: " + String(rpm));
-    ws.textAll("{\"rpm\":" + String(rpm) + ", \"dutycycle\":" + String(dutycycle) + "}");
-    mqtt_publish("fan/rpm", String(rpm));
-    mqtt_publish("fan/dutycycle", String(dutycycle));    
-//    ws.textAll("{\"rpm\":" + String(rpm) + ", \"dutycycle\":" + String(dutycycle) + ", \"freemem\":" + String(ESP.getFreeHeap())  + "}");
-  
-*/
-
-    // call sensors.requestTemperatures() to issue a global temperature 
-    // request to all devices on the bus
-    Serial.print("Requesting temperatures...");
-    sensors.requestTemperatures(); // Send the command to get temperatures
-    Serial.println("DONE");
-
+//    ws.textAll("{\"freemem\":" + String(ESP.getFreeHeap())  + "}");
     lastsendtime = millis();
-
   }
 
 }
